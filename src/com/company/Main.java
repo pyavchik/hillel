2ddf225e92064128a12d8b061a94197e00c3530e
pyavchik <pyavchik@gmail.com>
2016-02-09 20:35:02 +0200

    last commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,15 @@ package com.company;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, world!");
+        String s = "Hello world";
+
+        int birthday = 1984;
+        int age = 2016 - birthday;
+
+        System.out.println(s);
+        System.out.println(age);
+
     }
 }
