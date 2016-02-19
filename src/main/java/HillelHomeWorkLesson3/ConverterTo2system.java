770abc932b0917b60d323cc122e16b90aacf6076
DGovorov <heavenbox@mail.ru>
2016-02-19 18:54:32 +0200

    a little mstake in class name

--- src/main/java/HillelHomeWorkLesson3/ConverterTo2system.java
+++ src/main/java/HillelHomeWorkLesson3/ConverterTo2system.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 9. Перевести число в двоичную систему счисления (если не знаете что это можно не решать, объясню на занятии)
  *
  */
-public class ConverterTo2syster {
+public class ConverterTo2system {
 
     public static void main(String[] args) {
         System.out.println("What number do you want to convert?");
